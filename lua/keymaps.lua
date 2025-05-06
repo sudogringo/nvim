510723290b52b160a07ccf0eadc0f2420ce78c77
Tiago CB <16941297+YankeeDeMierda@users.noreply.github.com>
2025-05-06 14:31:27 -0300

    Added spellcheck keybing

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,6 +8,10 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Add language spell check
+vim.keymap.set('n', '<leader>dS', '<cmd>setlocal spell! spelllang=es<CR>', { desc = 'Spelling ES_UTF' })
+vim.keymap.set('n', '<leader>dE', '<cmd>setlocal spell! spelllang=en_us<CR>', { desc = 'Spelling EN_US' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
