c747791be10ce540055a1f5f38c55eb3dca3695d
Tiago CB <16941297+YankeeDeMierda@users.noreply.github.com>
2025-06-05 12:45:36 -0300

    Changed themes

--- init.lua
+++ init.lua
@@ -73,6 +73,5 @@ vim.api.nvim_create_autocmd('SpellFileMissing', {
     Download_spell_file(lang)
   end,
 })
-
 -- -- The line beneath this is called `modeline`. See `:help modeline`
 -- -- vim: ts=2 sts=2 sw=2 et
--- lua/kickstart/plugins/themes.lua
+++ lua/kickstart/plugins/themes.lua
@@ -1,14 +1,25 @@
 return {
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   {
-    'scottmckendry/cyberdream.nvim',
+    'yorik1984/newpaper.nvim',
+    priority = 1000,
+    config = true,
+  },
+  {
+    'rebelot/kanagawa.nvim',
     lazy = false,
     priority = 1000,
   },
   {
-    'yorik1984/newpaper.nvim',
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
     priority = 1000,
-    config = true,
+  },
+  {
+    'cpplain/flexoki.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
   },
   {
     'sainnhe/everforest',
@@ -16,14 +27,11 @@ return {
     priority = 1000,
     config = function()
       vim.g.everforest_enable_italic = true
-      vim.cmd.colorscheme 'everforest'
     end,
   },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 500, -- Make sure to load this before all the other start plugins.
     config = function()
@@ -33,11 +41,6 @@ return {
           comments = { italic = false }, -- Disable italics in comments
         },
       }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 }
